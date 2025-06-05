220b5d4b80a357612f04e6867052df7b40ecf890
SpartaCodingClub <spartacodingclub.kr@gmail.com>
2025-06-05 15:24:31 +0900

    Create UICollider.cs

---
+++ Assets/Scripts/UICollider.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UICollider : MonoBehaviour
+{
+    [SerializeField]
+    private Graphic graphic;
+
+    [SerializeField]
+    private Vector4 padding = new();
+}
